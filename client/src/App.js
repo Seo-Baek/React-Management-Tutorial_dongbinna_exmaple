e9a43fb03a0ffdf6a5aacca9de9136666da7ce3c
Seojin Baek <sjin1920@nsonesoft.com>
2021-03-31 17:27:27 +0900

    Add Customer Search Module

--- client/src/App.js
+++ client/src/App.js
@@ -107,14 +107,16 @@ class App extends React.Component {
     super(props);
     this.state = {
       customers : '',
-      completed : 0
+      completed : 0,
+      searchKeyword : ''
     }
   }
 
   stateRefresh = () => {
     this.setState({
       customers : '',
-      completed : 0
+      completed : 0,
+      searchKeyword : ''
     });
     this.callApi()
       .then(res => this.setState({customers : res}))
@@ -123,6 +125,21 @@ class App extends React.Component {
   }
 
   render(){
+    const filteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.user_name.indexOf(this.state.searchKeyword) > -1
+      });
+      return data.map((c) => {
+        return <Customer stateRefresh = {this.stateRefresh}
+                         key = {c.id}
+                         id = {c.id}
+                         image = {c.image}
+                         name = {c.user_name}
+                         birthday = {c.birthday}
+                         gender = {c.gender}
+                         job = {c.job} />
+      });
+    }
     const { classes } = this.props;
     const cellList = ['번호','프로필 이미지','이름','생년월일','직업','설정']
     return (
@@ -151,6 +168,9 @@ class App extends React.Component {
                   input: classes.inputInput,
                 }}
                 inputProps={{ 'aria-label': 'search' }}
+                name="searchKeyword"
+                value = {this.state.searchKeyword}
+                onChange = {this.handleSrhValueChange}
               />
             </div>
           </Toolbar>
@@ -173,20 +193,8 @@ class App extends React.Component {
             </TableHead>
             <TableBody>
               {
-                this.state.customers ? this.state.customers.map(c => {
-                  return (
-                    <Customer  
-                      key = {c.id}
-                      id ={c.id}
-                      image = {c.image}
-                      name = {c.user_name}
-                      birthday = {c.birthday}
-                      gender = {c.gender}
-                      job = {c.job}
-                      stateRefresh = {this.stateRefresh}
-                    />
-                  );
-                }) : 
+                this.state.customers ? 
+                  filteredComponents(this.state.customers) : 
                 <TableRow>
                   <TableCell colSpan="6" align="center">
                     <CircularProgress className = {classes.progress} variant="determinate"
@@ -218,6 +226,12 @@ class App extends React.Component {
   progress = () => {
     const { completed }  = this.state;
     this.setState({completed : completed >= 100 ? 0 : completed + 1});
+  }
+
+  handleSrhValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
   }
 
 }
